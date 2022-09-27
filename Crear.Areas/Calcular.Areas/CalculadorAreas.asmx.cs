fe988ddb388674ee34fde7f7d5a551baa891dd1f
jose_cueto <jose-cueto12@hotmail.com>
2022-09-26 23:02:51 -0500

    Creación de los metodos para calcular áreas

--- Crear.Areas/Calcular.Areas/CalculadorAreas.asmx.cs
+++ Crear.Areas/Calcular.Areas/CalculadorAreas.asmx.cs
@@ -22,5 +22,26 @@ namespace Calcular.Areas
         {
             return "Hola a todos";
         }
+
+        [WebMethod]
+        public decimal cuadrado(decimal n1)
+        {
+            Calculo c = new Calculo();
+            return c.cuadrado(n1);
+        }
+
+        [WebMethod]
+        public decimal triangulo(decimal n1, decimal n2)
+        {
+            Calculo c = new Calculo();
+            return c.triangulo(n1, n2);
+        }
+
+        [WebMethod]
+        public double circulo(double n1)
+        {
+            Calculo c = new Calculo();
+            return c.circulo(n1);
+        }
     }
 }
---
+++ Crear.Areas/Calcular.Areas/Calculo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Calcular.Areas
+{
+    public class Calculo
+    {
+        public decimal numero1 { get; set; }
+        public decimal numero2 { get; set; }
+
+        public decimal cuadrado(decimal n1)
+        {
+            decimal area;
+            area = n1 * n1;
+            return area;
+        }
+
+        public decimal triangulo(decimal n1, decimal n2)
+        {
+            decimal ba = n1;
+            decimal al = n2;
+            decimal area;
+            area = (ba * al) / 2;
+            return area;
+        }
+
+        public double circulo(double n1)
+        {
+            double radio = n1;
+            double area;
+            area = (radio * radio) * 3.1416;
+            return area;
+        }
+
+        
+    }
+}
